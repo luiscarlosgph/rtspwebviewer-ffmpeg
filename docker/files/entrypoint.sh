02d8fe3f7d42b98802e28e839774ee49e2269cfd
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2022-11-13 16:13:12 +0000

    Making the output of ffmpeg and python silent.

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -4,13 +4,13 @@
 source $USER/.zshrc
 
 # Run ffmpeg to convert the RTSP stream into HLS
-ffmpeg -fflags nobuffer -rtsp_transport tcp -i $RTSP -c copy -hls_time 2 -hls_wrap 10 /root/streaming.m3u8 &
+ffmpeg -fflags nobuffer -rtsp_transport tcp -i $RTSP -c copy -hls_time 2 -hls_wrap 10 /root/streaming.m3u8 > /dev/null 2>&1 &
 
 # Run Python web server
 if [ -z $WEB_PWD ]; then 
-    python3 -m rtspwebviewer_ffmpeg.run --address 0.0.0.0 --port $PORT --title $WEB_TITLE --m3u8 /root/streaming.m3u8 &
+    python3 -m rtspwebviewer_ffmpeg.run --address 0.0.0.0 --port $PORT --title $WEB_TITLE --m3u8 /root/streaming.m3u8 > /dev/null 2>&1 &
 else 
-    python3 -m rtspwebviewer_ffmpeg.run --address 0.0.0.0 --port $PORT --title $WEB_TITLE --m3u8 /root/streaming.m3u8 --password $WEB_PWD &
+    python3 -m rtspwebviewer_ffmpeg.run --address 0.0.0.0 --port $PORT --title $WEB_TITLE --m3u8 /root/streaming.m3u8 --password $WEB_PWD > /dev/null 2>&1 &
 fi
 
 
