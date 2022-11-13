c5e097d6e9258a03a15375cd04b2150eb6bd8f1f
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2022-11-13 15:46:51 +0000

    Modified entrypoint to run ffmpeg and the python package.

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Setup the environment
 source $USER/.zshrc
+
+# Run ffmpeg to convert the RTSP stream into HLS
+ffmpeg -fflags nobuffer -rtsp_transport tcp -i $RTSP -c copy -hls_time 2 -hls_wrap 10 /root/streaming.m3u8 &
+
+# Run Python web server
+python3 -m rtspwebviewer_ffmpeg.run --address 0.0.0.0 --port $PORT --title $WEB_TITLE --m3u8 /root/streaming.m3u8 --password $WEB_PWD &
 
 # Run the CMD specified by the user
 sh -c "$(echo $@)"
