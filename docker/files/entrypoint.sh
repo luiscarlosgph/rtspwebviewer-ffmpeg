e256948c120fedf0119806b0b5f2a0104b84f789
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2023-12-04 01:47:24 +0000

    Adding command to run Janus from boot.

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -6,6 +6,9 @@ source $USER/.zshrc
 # Run ffmpeg to convert the RTSP stream into RTP
 ffmpeg -i $RTSP -an -c:v copy -flags global_header -bsf dump_extra -f rtp rtp://localhost:5052 -vn -codec:a libopus -f rtp rtp://localhost:5051 > /dev/null 2>&1 &
 
+# Run Janus Gateway
+/app/backend/webrtc-server/bin/janus &
+
 # Run Python web server
 #if [ -z $WEB_PWD ]; then 
 #    python3 -m rtspwebviewer_ffmpeg.run --address 0.0.0.0 --port $PORT --title $WEB_TITLE --m3u8 /root/streaming.m3u8 > /dev/null 2>&1 &
